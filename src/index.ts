d752896b5e00d7d662fd55a9bb1bcff5f6f0a9c0
Rom Grk <romgrk.cc@gmail.com>
2023-02-10 18:59:55 -0500

    lint

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,12 @@ import './typedarray'
 import './graphics'
 
 import * as h from 'kui.legacy'
-import * as cairo from 'kui.cairo.cairo'
-import { Color } from 'src/color'
-import { Rectangle } from 'src/math'
 import { Renderer } from 'src/core'
 import { Container } from 'src/display'
 import { Graphics } from 'src/graphics'
 import { Text } from 'src/text'
 
 export function setup() {
-  // print(new Rectangle(1, 0, 10, 10))
-  // const image = cairo.image_surface('argb32', 100, 100)
-  // const context = image.context()
-
   const renderer = new Renderer({
     width: 150,
     height: 80,
@@ -42,14 +35,4 @@ export function setup() {
     renderer.canvasContext.rootContext.surface,
     { buffer: 0, row: 0, col: 0 }
   )
-
-  // renderer.canvasContext.rootContext
-  // renderer.canvasContext.rootContext
-  // print(vim.inspect(renderer))
 }
-
-// core.index
-// core.Renderer
-// canvas-renderer.index
-// canvas-renderer.CanvasContextSystem
-// core.index
