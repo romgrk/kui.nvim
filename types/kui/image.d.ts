9d754b1c860fea32fdacaed3497dc70b6ee3052b
Rom Grk <romgrk.cc@gmail.com>
2023-02-10 21:53:10 -0500

    lint

--- types/kui/image.d.ts
+++ types/kui/image.d.ts
@@ -6,8 +6,7 @@ declare module 'kui.legacy.image' {
     static new(data: any, opts: { buffer?: number, row: number, col: number }): Image;
 
     transmit(fn?: Function): void;
-    display(): void;
+    display(opts?: { z_index?: number }): void;
     delete(opts?: { free?: boolean }): void;
   }
-
 }
