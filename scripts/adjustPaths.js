dacc1a1384455dec05dbf5762e5266df1f6c0929
Rom Grk <romgrk.cc@gmail.com>
2023-02-12 23:26:30 -0500

    build: fix types

--- scripts/adjustPaths.js
+++ scripts/adjustPaths.js
@@ -3,7 +3,9 @@ const path = require('path')
 const walkdir = require('walkdir')
 
 const rootDist = path.join(__dirname, '../dist')
-const prefix = '\'src/'
+const srcPrefix = `from 'src/`
+const types1Prefix = `from 'types/`
+const types2Prefix = `/// <reference types="./types`
 
 const files = walkdir.sync(rootDist)
 
@@ -11,11 +13,23 @@ files.forEach(file => {
   if (fs.statSync(file).isDirectory())
     return
 
-  const content = fs.readFileSync(file).toString()
   const relativePath = file.replace(rootDist + '/', '')
   const levels = relativePath.split('/').length - 1
-  const replacementPath = Array.from({ length: levels }).map(() => '..').join('/') || '.'
-  const replacement = `'${replacementPath}/`
 
-  fs.writeFileSync(file, content.replaceAll(prefix, replacement))
+  let content = fs.readFileSync(file).toString()
+
+  const srcReplacement = `from '${dotsForLevel(levels)}/`
+  content = content.replaceAll(srcPrefix, srcReplacement)
+
+  const types1Replacement = `from '${dotsForLevel(levels + 1)}/`
+  content = content.replaceAll(types1Prefix, types1Replacement)
+
+  const types2Replacement = `/// <reference types="${dotsForLevel(levels + 1)}/types`
+  content = content.replaceAll(types2Prefix, types2Replacement)
+
+  fs.writeFileSync(file, content)
 })
+
+function dotsForLevel(levels) {
+  return Array.from({ length: levels }).map(() => '..').join('/') || '.'
+}
